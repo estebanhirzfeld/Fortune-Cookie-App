52fd4834f009f4810fe34c75839e9b0574159833
steve-m1 <estebanhirzfeld@gmail.com>
2025-07-24 17:57:44 -0300

    feat: add cookie model

--- backend/app/Models/Cookie.php
+++ backend/app/Models/Cookie.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cookie extends Model
 {
-    //
+    protected $fillable = [
+        'message',
+        'timestamp',
+        'creator',
+    ];
+
+    /**
+     * Get the user who created the cookie.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'creator');
+    }
 }
